48e16c806706cb12a3a98b21c0f680fef30ab6bb
Abd-Allatif <abd8allatif8@gmail.com>
2026-09-09 03:17:52 -0400

    Connected to the database and Initialized the routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,7 +7,6 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -21,9 +20,8 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
-// Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+giftRoutes = require('./routes/giftRoutes');
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,3 +1,5 @@
+const { MongoError } = require('mongodb');
+
 // db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
@@ -9,20 +11,30 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
+    if (dbInstance) {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    try {
+        await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+        console.log("Connected to the database");
 
-    // Task 3: Return database instance
-    // {{insert code}}
+        dbInstance = client.db(dbName);
+
+        if(!dbInstance){
+            throw new Error('Database not initialized. Call connect first.');
+            return;
+        }
+
+        console.log("dbInstance Initialized");
+    } catch (err) {
+        console.log("There was a problem connecting to the database" + err);
+    }
+
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
